73e2f221f9bc74be58b572cc03a63aac60a6d64a
holyaustin <holyaustin@yahoo.com>
2024-06-06 17:15:29 +0100

    explore

--- packages/nextjs/app/explore/page.jsx
+++ packages/nextjs/app/explore/page.jsx
@@ -1,24 +1,179 @@
 "use client";
 
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
 import RealEstateFractionalize from "../../artifacts/contracts/RealEstateFractionalize.sol/RealEstateFractionalize.json";
 import { liskAddress } from "../../config";
-// import axios from "axios";
+import axios from "axios";
 // import { bnbAddress } from "../../config";
 import "dotenv/config";
 import { ethers } from "ethers";
-import { NFTStorage } from "nft.storage";
+//import { NFTStorage } from "nft.storage";
 import Web3Modal from "web3modal";
-import { getTargetNetworks } from "~~/utils/scaffold-eth";
 
-const allowedNetworks = getTargetNetworks();
+//import { getTargetNetworks } from "~~/utils/scaffold-eth";
+
 const fileShareAddress = liskAddress;
 
-// const APIKEY = [process.env.NFT_STORAGE_API_KEY];
-const APIKEY =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweDA4Zjc4ODAwMkUzZDAwNEIxMDI3NTFGMUQ0OTJlNmI1NjNFODE3NmMiLCJpc3MiOiJuZnQtc3RvcmFnZSIsImlhdCI6MTY1MzA1NjE4NzM4MCwibmFtZSI6InBlbnNpb25maSJ9.agI-2V-FeK_eVRAZ-T6KGGfE9ltWrTUQ7brFzzYVwdM";
+export default function Explore() {
+  const router = useRouter();
+  const [nfts, setNfts] = useState([]);
+  const [loadingState, setLoadingState] = useState("not-loaded");
+  useEffect(() => {
+    loadfileNFT();
+  }, []);
 
+  // const rpcUrl = "https://rpc.sepolia-api.lisk.com";
+  // const rpcUrl = "http://localhost:8545";
+
+  async function loadfileNFT() {
+    const web3Modal = new Web3Modal({
+      network: "mainnet",
+      cacheProvider: true,
+    });
+    const connection = await web3Modal.connect();
+    const provider = new ethers.providers.Web3Provider(connection);
+    const signer = provider.getSigner();
+    const contract = new ethers.Contract(fileShareAddress, RealEstateFractionalize.abi, signer);
+    console.log("contract is ", contract);
+    console.log("data loading ");
+    const data = await contract.fetchMyPropertyRegistered();
+    console.log("data fetched is ", data);
+    console.log("Testing Index of Property Struct ", contract.properties(0));
+    /*
+     *  map over items returned from smart contract and format
+     *  them as well as fetch their token metadata
+     */
+    const items = await Promise.all(
+      data.map(async i => {
+        const tokenUri = await contract.properties(i.id);
+        console.log("token Uri is ", tokenUri);
+        const httpUri = getIPFSGatewayURL(tokenUri);
+        console.log("Http Uri is ", httpUri);
+        const meta = await axios.get(httpUri);
+
+        const item = {
+          tokenId: i.id.toNumber(),
+          image: getIPFSGatewayURL(meta.data.image),
+          name: meta.data.name,
+          description: meta.data.description,
+          sharelink: getIPFSGatewayURL(meta.data.image),
+          pin: meta.data.properties.pin,
+          paddress: meta.data.properties.paddress,
+          ptype: meta.data.properties.ptype,
+          price: meta.data.properties.price,
+          totalfraction: meta.data.properties.totalfraction,
+          image2: getIPFSGatewayURL(meta.data.properties.image2),
+          image3: getIPFSGatewayURL(meta.data.properties.image3),
+        };
+        console.log("item returned is ", item);
+
+        return item;
+      }),
+    );
+
+    setNfts(items);
+    setLoadingState("loaded");
+  }
+  const getIPFSGatewayURL = ipfsURL => {
+    const urlArray = ipfsURL.toString().split("/");
+    console.log("urlArray = ", urlArray);
+    const ipfsGateWayURL = `https://${urlArray[2]}.ipfs.nftstorage.link/${urlArray[3]}`;
+    console.log("ipfsGateWayURL = ", ipfsGateWayURL);
+    return ipfsGateWayURL;
+  };
+
+  async function share(nft) {
+    console.log("item id clicked is", nft.tokenId);
+    const id = nft.tokenId;
+
+    router.push({
+      pathname: "/carDetails",
+      query: { id },
+    });
+    console.log("Prop result without {} is ", { id });
+
+    /**
+      const web3Modal = new Web3Modal();
+      const connection = await web3Modal.connect();
+      const provider = new ethers.providers.Web3Provider(connection);
+      const signer = provider.getSigner();
+      const contract = new ethers.Contract(fileShareAddress, RealEstateFractionalize.abi, signer);
+  
+      const transaction = await contract.createFileShare(nft.tokenId);
+      await transaction.wait();
+      console.log("RealEstateFractionalize Share transaction completed ");
+      const token = nft.tokenId;
+      console.log("token id is ", token);
+      loadfileNFT();
+       */
+    //navigate("/view", { state: token });
+  }
+  /*
+  async function CarDetails() {
+    router.push({
+      pathname: "/carDetails",
+      query: { id },
+    });
+    console.log("Prop result is ", prop.id);
+  }
+  */
+  if (loadingState === "loaded" && !nfts.length) {
+    return (
+      <div sx={styles.section}>
+        <h1 className="px-20 py-10 text-3xl text-white">Empty drive, no file yet</h1>
+      </div>
+    );
+  }
+  return (
+    <div>
+      <div className="bg-blue-100 text-4xl text-center text-black font-bold pt-10">
+        <h1>Available Properties</h1>
+      </div>
+      <div className="flex justify-center bg-blue-100 mb-12">
+        <div className="px-4" style={{ maxWidth: "1600px" }}>
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 pt-4">
+            {nfts.map((nft, i) => (
+              <div key={i} className="shadow rounded-xl overflow-hidden border-2 border-white-500">
+                <img
+                  title="RealEstateFractionalize"
+                  frameBorder="0"
+                  scrolling="no"
+                  height="200px"
+                  width="100%"
+                  // objectFit="cover"
+                  src={`${nft.image}#toolbar=0`}
+                  className="py-3 object-cover h-500"
+                  w={nft.key}
+                />
+                <div className="p-1">
+                  <p style={{ height: "34px" }} className="text-xl text-purple-700 font-semibold">
+                    Name: {nft.name}
+                  </p>
+                  <p className="text-xl font-bold text-black">ID : {nft.tokenId}</p>
+                  <p className="text-xl font-bold text-black">PIN : {nft.pin}</p>
+                </div>
+                {/** onClick={() => share(nft)} */}
+                <div className="p-2 bg-black">
+                  <button
+                    data-value={nft}
+                    type="button"
+                    onClick={() => share(nft)}
+                    className="w-full bg-purple-700 text-white font-bold py-2 px-2 rounded"
+                  >
+                    View Property
+                  </button>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+/**
 const Register = () => {
   const navigate = useRouter();
   const [errorMessage, setErrorMessage] = useState(null);
@@ -174,13 +329,13 @@ const Register = () => {
     //4. navigate("/explore");
     navigate.push("/buy");
   };
-  /*
+
   async function CarDetails() {
    Link to Library Categories 
     // router.push("/catebooks");
     navigate("/carDetails");
   }
-  */
+ 
 
   const getIPFSGatewayURL = ipfsURL => {
     const urlArray = ipfsURL.split("/");
